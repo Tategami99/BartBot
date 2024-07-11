8cefaac196d36d66f61386d5e27d8649351b1670
Kaden Bautista <k07bautista@gmail.com>
2024-07-11 11:44:07 -0700

    added basic outline for project

--- BartBot.ino
+++ BartBot.ino
@@ -1,9 +1,36 @@
+
+enum States {
+  FORWARD,
+  TURN,
+  STOP
+};
+
+States state = States::STOP;
+
+void turnRight(unsigned long duration) {
+  //TODO: move motors in opposite directions
+  delay(duration);
+  //TODO: stop motors
+}
+
+void turnRight(unsigned long duration) {
+  //TODO: move motors in opposite directions
+  delay(duration);
+  //TODO: stop motors
+}
+
 void setup() {
   // put your setup code here, to run once:
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  switch(state) {
+    case FORWARD:
+      break;
+    case TURN:
+      break;
+    case STOP:
+      break;
+  }
 }
