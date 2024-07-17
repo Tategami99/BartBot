705bd2e61a0c55fb14c5ebc8adae28d85e0e0f12
Kaden Bautista <k07bautista@gmail.com>
2024-07-16 23:26:28 -0700

    using speaker and sd module

--- BartBot.ino
+++ BartBot.ino
@@ -1,12 +1,50 @@
-#define BUZZER_PIN 9
+#include <SD.h>
+#include <TMRpcm.h>
+#include <pcmConfig.h>
+#include <pcmRF.h>
+
+enum Songs {
+  HAPPY_BIRTHDAY,
+  OMG,
+  ITS_NOT_LIKE_I_LIKE_YOU
+};
+Songs song = Songs::HAPPY_BIRTHDAY;
+
+//SD DEFINITIONS
+#define SD_CS_PIN 2
+
+//DEFINITIONS & VARIABLES
+#define SPEAKER_PIN 3
+TMRpcm tmrpcm;
 
 void setup() {
-  pinMode(BUZZER_PIN, OUTPUT);
+  Serial.begin(9600);
+
+  //SD CARD
+  SD.begin(SD_CS_PIN);
+  
+  //AUDIO
+  tmrpcm.speakerPin = SPEAKER_PIN;
+  tmrpcm.quality(0);
 }
 
 void loop() {
-  tone(BUZZER_PIN, 1000);
-  delay(500);
-  noTone(BUZZER_PIN);
-  delay(500);
+  //TODO: adjust volume for specific songs 
+  switch(song) {
+    case HAPPY_BIRTHDAY:
+      tmrpcm.stopPlayback();
+      tmrpcm.setVolume(5);
+      tmrpcm.play("happy_birthday.mp3");
+      break;
+    case OMG:
+      tmrpcm.stopPlayback();
+      tmrpcm.setVolume(5);
+      tmrpcm.play("omg.mp3");
+      break;
+    case ITS_NOT_LIKE_I_LIKE_YOU:
+      tmrpcm.stopPlayback();
+      tmrpcm.setVolume(5);
+      tmrpcm.play("its_not_like_i_like_you.mp3");
+      break;
+  }
 }
