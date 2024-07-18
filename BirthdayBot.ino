42c09b2900a409a62b98d2de534bfc838ef3f745
Kaden Bautista <k07bautista@gmail.com>
2024-07-17 23:00:22 -0700

    servo matches song length

--- BirthdayBot.ino
+++ BirthdayBot.ino
@@ -1,3 +1,5 @@
+#include <SPI.h>
+#include <MFRC522.h>
 #include <Servo.h>
 #include <SD.h>
 #include <TMRpcm.h>
@@ -5,9 +7,9 @@
 #include <pcmRF.h>
 
 enum Songs {
-  HAPPY_BIRTHDAY = 30,
-  OMG = 60,
-  ITS_NOT_LIKE_I_LIKE_YOU = 90
+  HAPPY_BIRTHDAY = 130000,
+  OMG = 140000,
+  ITS_NOT_LIKE_I_LIKE_YOU = 150000
 };
 Songs song = Songs::HAPPY_BIRTHDAY;
 
@@ -16,11 +18,19 @@ Songs song = Songs::HAPPY_BIRTHDAY;
 
 //AUDIO DEFINITIONS & VARIABLES
 #define SPEAKER_PIN 3
-TMRpcm tmrpcm;
+TMRpcm speaker;
+unsigned long musicStartTime = 0;
+bool musicPlaying = false;
+unsigned long currentSongDuration = 0;
 
 //SERVO DEFINITIONS & VARIABLES
 #define SERVO_PIN 4
 Servo arm;
+
+//RFID DEFINITIONS & VARIABLES
+#define RST_PIN 8
+#define SS_PIN 9
+MFRC522 reader(SS_PIN, RST_PIN);
 
 void setup() {
   Serial.begin(9600);
@@ -29,36 +39,58 @@ void setup() {
   SD.begin(SD_CS_PIN);
   
   //AUDIO
-  tmrpcm.speakerPin = SPEAKER_PIN;
-  tmrpcm.quality(0);
+  speaker.speakerPin = SPEAKER_PIN;
+  speaker.quality(0);
 
   //SERVO
   arm.attach(SERVO_PIN);
+
+  //RFID
+  SPI.begin();
+  reader.PCD_Init();
 }
 
 void loop() {
   //TODO: adjust volume for specific songs 
   switch(song) {
     case HAPPY_BIRTHDAY:
-      arm.write(Songs::HAPPY_BIRTHDAY);
       delay(1000);
-      tmrpcm.stopPlayback();
-      tmrpcm.setVolume(5);
-      tmrpcm.play("happy_birthday.mp3");
+      playAudioFile("happy_birthday.mp3", Songs::HAPPY_BIRTHDAY, 5);
       break;
     case OMG:
-      arm.write(Songs::OMG);
       delay(1000);
-      tmrpcm.stopPlayback();
-      tmrpcm.setVolume(5);
-      tmrpcm.play("omg.mp3");
+      playAudioFile("omg.mp3", Songs::OMG, 5);
       break;
     case ITS_NOT_LIKE_I_LIKE_YOU:
-      arm.write(Songs::ITS_NOT_LIKE_I_LIKE_YOU);
       delay(1000);
-      tmrpcm.stopPlayback();
-      tmrpcm.setVolume(5);
-      tmrpcm.play("its_not_like_i_like_you.mp3");
+      playAudioFile("its_not_like_i_like_you.mp3", Songs::ITS_NOT_LIKE_I_LIKE_YOU, 5);
       break;
   }
+
+  updateArm(100);
+}
+
+void playAudioFile(const char* filename, unsigned long duration, unsigned int volume) {
+  speaker.stopPlayback();
+  arm.write(0);
+  currentSongDuration = duration;
+  musicStartTime = millis();
+  musicPlaying = true;
+
+  speaker.setVolume(volume);
+  speaker.play(filename);
+}
+
+void updateArm(unsigned long updateDelay) {
+  unsigned long elapsedTime = millis() - musicStartTime;
+
+  int angle = map(elapsedTime, 0, currentSongDuration, 0, 180);
+  arm.write(angle);
+
+  if(!speaker.isPlaying()) {
+    musicPlaying = false;
+    arm.write(180);
+  }
+
+  delay(updateDelay);
 }
